707ba8a415b8653cd4dc1dc032e9fd0e4073b280
aglassoforange <yliu2911@gmail.com>
2023-03-08 19:51:56 -0800

    crud

--- src/App.js
+++ src/App.js
@@ -11,74 +11,97 @@ import {
 } from "firebase/firestore";
 
 function App() {
-  const [newName, setNewName] = useState("");
-  const [newAge, setNewAge] = useState(0);
+  const [newTitle, setTitle] = useState("");
+  const [newLocation, setLocation] = useState("");
+  const [newBody, setBody] = useState("");
+  const [newDate, setDate] = useState(Date.now());
 
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  const [instances, setInstances] = useState([]);
+  const instanceRef = collection(db, "instance");
 
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  const createInstance = async () => {
+    await addDoc(instanceRef, { title: newTitle, location: newLocation, body: newBody, time: newDate });
   };
 
-  const updateUser = async (id, age) => {
-    const userDoc = doc(db, "users", id);
-    const newFields = { age: age + 1 };
+  const updateInstance = async (id,body) => {
+    const userDoc = doc(db, "instances", id);
+    const newFields = { body: body };
     await updateDoc(userDoc, newFields);
   };
 
-  const deleteUser = async (id) => {
-    const userDoc = doc(db, "users", id);
-    await deleteDoc(userDoc);
-  };
 
+  //delet doc 后面的是instance 也就是我们database中的名称
+  const deleteInstance = async (id) => {
+    const userDoc = doc(db, "instance", id);
+    console.log(id);
+    await deleteDoc(userDoc);
+
+    // const docRef = doc(db, 'instances', id);
+    // await deleteDoc(docRef)
+
+  };
+  //this is happening when the page is rendered
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    const getInstance = async () => {
+      const data = await getDocs(instanceRef);
+      setInstances(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
-    getUsers();
+    getInstance();
   }, []);
 
   return (
     <div className="App">
       <input
-        placeholder="Name..."
+        placeholder="Title..."
         onChange={(event) => {
-          setNewName(event.target.value);
+          setTitle(event.target.value);
         }}
       />
       <input
-        type="number"
-        placeholder="Age..."
+        placeholder="Location..."
         onChange={(event) => {
-          setNewAge(event.target.value);
+          setLocation(event.target.value);
+        }}
+      />
+      <input
+        placeholder="Body..."
+        onChange={(event) => {
+          setBody(event.target.value);
+        }}
+      />
+      <input
+        type="Date"
+        placeholder="Date..."
+        onChange={(event) => {
+          setDate(event.target.value);
         }}
       />
 
-      <button onClick={createUser}> Create User</button>
-      {users.map((user) => {
+      <button onClick={createInstance}> Create User</button>
+      {instances.map((one_instance) => {
         return (
           <div>
-            {" "}
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
+            <h1>title: {one_instance.title}</h1>
+            {/* <h1 >time:</h1>
+            <h1>location: {one_instance.location}</h1>
+            <h1>body: {one_instance.body}</h1> */}
+
             <button
               onClick={() => {
-                updateUser(user.id, user.age);
+                updateInstance(one_instance.id,one_instance.body);
               }}
             >
               {" "}
-              Increase Age
+              updateInstance
             </button>
             <button
               onClick={() => {
-                deleteUser(user.id);
+                deleteInstance(one_instance.id);
               }}
             >
               {" "}
-              Delete User
+              Delete
             </button>
           </div>
         );
